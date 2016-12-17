883f6165a6764d7762efe8119c6aca242a383e31
qifeiqin <qifeiqin@gmail.com>
2016-12-17 16:48:15 +0800

    使用 WMPageController实现android的viewpager效果

--- RongCloudDemo/OnlineReadingViewController.h
+++ RongCloudDemo/OnlineReadingViewController.h
@@ -7,7 +7,11 @@
 //
 
 #import <UIKit/UIKit.h>
+//导入viewpager的库
+//http://blog.csdn.net/yubo_725/article/details/51159633
+#import "WMPageController.h"
+@interface OnlineReadingViewController :  WMPageController
 
-@interface OnlineReadingViewController : UIViewController
-
+#pragma mark 标题数组
+@property (strong, nonatomic) NSArray *titles;
 @end
--- RongCloudDemo/OnlineReadingViewController.m
+++ RongCloudDemo/OnlineReadingViewController.m
@@ -14,41 +14,79 @@
 
 @implementation OnlineReadingViewController
 
+#pragma mark 初始化代码
+- (instancetype)initWithCoder:(NSCoder *)aDecoder {
+    self = [super initWithCoder:aDecoder];
+    if(self) {
+        self.menuHeight = 35;
+        self.menuItemWidth = 100;
+        self.menuViewStyle = WMMenuViewStyleLine;
+        self.titles = [NSArray arrayWithObjects:@"娱乐新闻", @"体育新闻", nil];
+    }
+    return self;
+}
+
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+#pragma mark 返回index对应的标题
+- (NSString *)pageController:(WMPageController *)pageController titleAtIndex:(NSInteger)index {
+    return [self.titles objectAtIndex:index];
+}
+
+#pragma mark 返回子页面的个数
+- (NSInteger)numbersOfChildControllersInPageController:(WMPageController *)pageController {
+    return [self.titles count];
+}
+
+#pragma mark 返回某个index对应的页面，该页面从Storyboard中获取
+- (UIViewController *)pageController:(WMPageController *)pageController viewControllerAtIndex:(NSInteger)index {
+    UIStoryboard *storyboard = [UIStoryboard storyboardWithName:@"Main" bundle:nil];
+    UIViewController *controller1 = [storyboard instantiateViewControllerWithIdentifier:@"vp1"]; //这里的identifer是我们之前设置的StoryboardID
+    
+    UIViewController *controlle2 = [storyboard instantiateViewControllerWithIdentifier:@"vp2"]; //这里的identifer是我们之前设置的StoryboardID
+    
+    if(index==1)
+        
+        return controller1;
+    else
+        return controlle2;
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
-- (IBAction)btnEnterArticleDetail:(id)sender {
-    
-    
-    //根据storyboard id来获取目标页面
-    ArticleDetailViewController *nextPage= [self.storyboard instantiateViewControllerWithIdentifier:@"ArticleDetailViewController"];
-    
-    
-    //    传值
-    //UITabBarController和的UINavigationController结合使用,进入新的页面的时候，隐藏主页tabbarController的底部栏
-    nextPage.hidesBottomBarWhenPushed=YES;
-    
-    //跳转
-    [self.navigationController pushViewController:nextPage animated:YES];
-
-    
-    
-}
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
+//- (IBAction)btnEnterArticleDetail:(id)sender {
+//
+//
+//    //根据storyboard id来获取目标页面
+//    ArticleDetailViewController *nextPage= [self.storyboard instantiateViewControllerWithIdentifier:@"ArticleDetailViewController"];
+//
+//
+//    //    传值
+//    //UITabBarController和的UINavigationController结合使用,进入新的页面的时候，隐藏主页tabbarController的底部栏
+//    nextPage.hidesBottomBarWhenPushed=YES;
+//
+//    //跳转
+//    [self.navigationController pushViewController:nextPage animated:YES];
+//
+//
+//
+//}
+
+/*
+ #pragma mark - Navigation
+ 
+ // In a storyboard-based application, you will often want to do a little preparation before navigation
+ - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+ // Get the new view controller using [segue destinationViewController].
+ // Pass the selected object to the new view controller.
+ }
+ */
+
+
