fe8ca95a28165adbde404f5e3ea3d735dce8b1b3
qifeiqin <qifeiqin@gmail.com>
2016-12-18 14:43:02 +0800

    ConnectRongyunViewController /**  *这个类不能被删除，因为在appDelegate文件中有一个检测网络状态的函数，当用户掉线，程序会启动这个界面让  *用户重新登录  */

--- RongCloudDemo/ConnectRongyunViewController.h
+++ RongCloudDemo/ConnectRongyunViewController.h
@@ -2,7 +2,10 @@
 
 #import <UIKit/UIKit.h>
 #import <RongIMKit/RongIMKit.h>
-
+/**
+ *这个类不能被删除，因为在appDelegate文件中有一个检测网络状态的函数，当用户掉线，程序会启动这个界面让
+ *用户重新登录
+ */
 @interface ConnectRongyunViewController : UIViewController<RCIMUserInfoDataSource>
 
 
