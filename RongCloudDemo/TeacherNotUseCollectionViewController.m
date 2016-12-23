435f6ce332b52d59ccf9d5642bc24bc66bdf832e
qifeiqin <qifeiqin@gmail.com>
2016-12-23 22:57:26 +0800

    使用list代替table搞定

--- RongCloudDemo/TeacherNotUseCollectionViewController.m
+++ RongCloudDemo/TeacherNotUseCollectionViewController.m
@@ -14,18 +14,30 @@
 
 @implementation TeacherNotUseCollectionViewController{
     
-    NSMutableArray *recipes;
+    NSMutableArray *dataInOnerow;
+     NSMutableArray *dataInOnerow2;
+    NSMutableArray *allData;
+
     
 }
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    recipes=[[NSMutableArray alloc]init];
-    [recipes addObject:@"由于下大雪，今晚不用上课"];
-    [recipes addObject:@"由于下大雪，今晚不用上课"];
-    [recipes addObject:@"由于下大雪，今晚不用上课"];
-    //    recipes = [NSArray arrayWithObjects:@"Egg Benedict",@"Ham and Cheese Panini","yuzhiyun",nil];
-    // Do any additional setup after loading the view.
+    allData=[[NSMutableArray alloc]init];
+    
+    dataInOnerow=[[NSMutableArray alloc]init];
+    [dataInOnerow addObject:@"数学"];
+    [dataInOnerow addObject:@"98"];
+    [dataInOnerow addObject:@"02"];
+    
+    dataInOnerow2=[[NSMutableArray alloc]init];
+    [dataInOnerow2 addObject:@"英语"];
+    [dataInOnerow2 addObject:@"80"];
+    [dataInOnerow2 addObject:@"35"];
+    
+    [allData addObject:dataInOnerow];
+    [allData addObject:dataInOnerow2];
+    
 }
 
 - (void)didReceiveMemoryWarning {
@@ -36,7 +48,7 @@
 
 -(NSInteger) tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
     
-    return [recipes count];
+    return [allData count]+1;
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
@@ -49,8 +61,6 @@
     //        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:simpleTableIdentifier];
     //    }
     if (cell == nil) {
-        
-        
         cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleValue1 reuseIdentifier:simpleTableIdentifier];
     }
     
@@ -59,14 +69,34 @@
 //    //    cell.tes
 //    
 //    cell.textLabel.text = [recipes objectAtIndex:indexPath.row];
-    
+    //第一行显示文字，不显示具体的数据
+   
     UILabel *mUILabelsubject=(UILabel *)[cell viewWithTag:1];
-    mUILabelsubject.text=@"学科";
-
+    UILabel *mUILabelScore=(UILabel *)[cell viewWithTag:2];
+    UILabel *mUILabelRank=(UILabel *)[cell viewWithTag:3];
     
-    UILabel *mUILabelsubject2=(UILabel *)[cell viewWithTag:2];
-    mUILabelsubject.text=@"成绩";
+    if(0==indexPath.row){
+        mUILabelsubject.text=@"学科";
+         mUILabelScore.text=@"成绩";
+         mUILabelRank.text=@"排名";
+        
+        mUILabelsubject.font=[UIFont systemFontOfSize:20];
+        [mUILabelsubject setTextColor:[UIColor blackColor]];
+        
+        mUILabelScore.font=[UIFont systemFontOfSize:20];
+        [mUILabelScore setTextColor:[UIColor blackColor]];
 
+        mUILabelRank.font=[UIFont systemFontOfSize:20];
+        [mUILabelRank setTextColor:[UIColor blackColor]];
+
+
+    }else{
+        mUILabelsubject.text=[[allData objectAtIndex:indexPath.row-1] objectAtIndex:0];
+        mUILabelScore.text=[[allData objectAtIndex:indexPath.row-1] objectAtIndex:1];
+    mUILabelRank.text=[[allData objectAtIndex:indexPath.row-1] objectAtIndex:2];
+    }
+    
+    
     return cell;
 }
 
