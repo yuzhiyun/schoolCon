86659bc619ffaf28368aac9fc7d04cbd2b98dd24
qifeiqin <qifeiqin@gmail.com>
2017-01-12 10:52:31 +0800

    咨询师模块添加下拉刷新模块

--- RongCloudDemo/ConsultTableViewController.m
+++ RongCloudDemo/ConsultTableViewController.m
@@ -7,17 +7,91 @@
 //
 
 #import "ConsultTableViewController.h"
-
+#import "MJRefresh.h"
 @interface ConsultTableViewController ()
 
 @end
 
-@implementation ConsultTableViewController
+@implementation ConsultTableViewController{
+NSMutableArray *mDataConsult;
+
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
+      mDataConsult=[[NSMutableArray alloc]init];
+    [mDataConsult addObject:@"初始数据"];
+    [mDataConsult addObject:@"初始数据"];
+    [mDataConsult addObject:@"初始数据"];
+    [mDataConsult addObject:@"初始数据"];
+    [mDataConsult addObject:@"初始数据"];
+    
+    
+    // 2.集成刷新控件
+    [self setupRefresh];
 
 }
+/**
+ *  集成刷新控件
+ */
+- (void)setupRefresh
+{
+    // 1.下拉刷新(进入刷新状态就会调用self的headerRereshing)
+    //    [self.tableView addHeaderWithTarget:self action:@selector(headerRereshing)];
+    // dateKey用于存储刷新时间，可以保证不同界面拥有不同的刷新时间
+    [self.tableView addHeaderWithTarget:self action:@selector(headerRereshing) dateKey:@"table"];
+#warning 自动刷新(一进入程序就下拉刷新)
+//    [self.tableView headerBeginRefreshing];
+    
+    // 2.上拉加载更多(进入刷新状态就会调用self的footerRereshing)
+    [self.tableView addFooterWithTarget:self action:@selector(footerRereshing)];
+    
+    // 设置文字(也可以不设置,默认的文字在MJRefreshConst中修改)
+    self.tableView.headerPullToRefreshText = @"下拉可以刷新了";
+    self.tableView.headerReleaseToRefreshText = @"松开马上刷新了";
+    self.tableView.headerRefreshingText = @"正在为您刷新。。。";
+    
+    self.tableView.footerPullToRefreshText = @"上拉可以加载更多数据了";
+    self.tableView.footerReleaseToRefreshText = @"松开马上加载更多数据了";
+    self.tableView.footerRefreshingText = @"正在为您刷新。。。";
+}
+
+#pragma mark 开始进入刷新状态
+- (void)headerRereshing
+{
+    // 1.添加假数据
+    for (int i = 0; i<5; i++) {
+        [mDataConsult insertObject:@"下拉刷新数据" atIndex:0];
+    }
+    
+    // 2.模拟2秒后刷新表格UI（真实开发中，可以移除这段gcd代码）
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(2.0 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+        // 刷新表格
+        [self.tableView reloadData];
+        
+        // (最好在刷新表格后调用)调用endRefreshing可以结束刷新状态
+        [self.tableView headerEndRefreshing];
+    });
+}
+
+- (void)footerRereshing
+{
+    // 1.添加假数据
+    for (int i = 0; i<5; i++) {
+          [mDataConsult addObject:@"上拉刷新数据"];
+    }
+    
+    // 2.模拟2秒后刷新表格UI（真实开发中，可以移除这段gcd代码）
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(2.0 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+        // 刷新表格
+        [self.tableView reloadData];
+        
+        // (最好在刷新表格后调用)调用endRefreshing可以结束刷新状态
+        [self.tableView footerEndRefreshing];
+    });
+}
+
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
@@ -29,7 +103,7 @@
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
 #warning Incomplete implementation, return the number of rows
-    return 5;
+    return [mDataConsult count];
 }
 
 
@@ -52,7 +126,8 @@
     mUILabelTitle.text=@"一级心理咨询师";
     
     UILabel *mUILabelSpecialty=(UILabel *)[cell viewWithTag:3];
-mUILabelSpecialty.text=@"认知行为治疗";
+//mUILabelSpecialty.text=@"认知行为治疗";
+    mUILabelSpecialty.text=[mDataConsult objectAtIndex:indexPath.row];
     return cell;
 }
 
