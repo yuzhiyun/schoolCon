d8216918c1349437f8bbbe004d0e66646f034a47
qifeiqin <qifeiqin@gmail.com>
2017-01-17 15:04:43 +0800

    活动

--- RongCloudDemo/ShalongTableViewController.m
+++ RongCloudDemo/ShalongTableViewController.m
@@ -13,6 +13,18 @@
 #import "Activity.h"
 #import "MJRefresh.h"
 #import "UIImageView+WebCache.h"
+#import "ArticleTableViewController.h"
+#import "Vp1TableViewCell.h"
+#import "ArticleDetailViewController.h"
+#import "AppDelegate.h"
+#import "WMPageController.h"
+#import "UIImageView+WebCache.h"
+#import "AFNetworking.h"
+#import "AppDelegate.h"
+#import "MBProgressHUD.h"
+#import "Article.h"
+#import "JsonUtil.h"
+#import "MJRefresh.h"
 @interface ShalongTableViewController ()
 
 @end
@@ -20,10 +32,19 @@
 @implementation ShalongTableViewController{
     NSMutableArray *allDataFromServer;
     
+    UITableView *mTableView;
+    //页面索引，分页查询数据，下拉刷新的时候需要使用到
+    int pageIndex;
+
 }
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
+    //数据默认为先加载第一页
+    pageIndex=1;
+    [self loadData:pageIndex orientation:@"up"];
+    
     self.title=@"岳麓沙龙";
     [self.navigationController.navigationBar setBarTintColor:[UIColor colorWithRed:3/255.0 green:121/255.0 blue:251/255.0 alpha:1.0]];
     [self.navigationController.navigationBar setTitleTextAttributes:[NSDictionary dictionaryWithObjectsAndKeys:[UIColor whiteColor],UITextAttributeTextColor,nil]];
@@ -31,45 +52,32 @@
     self.navigationItem.backBarButtonItem = [[UIBarButtonItem alloc] initWithTitle:@"返回" style:UIBarButtonItemStylePlain target:nil action:nil];
     //防止与顶部重叠
 //    self.tableView.contentInset=UIEdgeInsetsMake(20.0f, 0.0f, 0.0f, 0.0f);
-    
-//    //指定大标题
-//    mData=[[NSMutableArray alloc]init];
-//    [mData addObject:@"长沙第一次岳麓活动"];
-//    [mData addObject:@"亲子军营模拟活动"];
-//    [mData addObject:@"第一届读书交流会"];
-//    [mData addObject:@"第五届高校马拉松比赛"];
-//    [mData addObject:@"2016年度百里毅行活动"];
-//    //指定封面
-//    mImg=[[NSMutableArray alloc]init];
-//    [mImg addObject:@"a1.png"];
-//    [mImg addObject:@"a2.jpg"];
-//    [mImg addObject:@"a3.jpg"];
-//    [mImg addObject:@"a4.jpg"];
-//    [mImg addObject:@"a5.jpg"];
-    Activity *model=[[Activity alloc]init];
-    model.activityId=@"1";
-    model.picUrl=@"http://mmbiz.qpic.cn/mmbiz_jpg/6qu8KwIJTLcqTxT8jqPOKCEAvGuhbFpzscsPC8FeDEYkQxsQ1AFSeJjYWgMeicQU2gJrgb3bhu7quicok24sLwIw/640?wx_fmt=jpeg&wxfrom=5&wx_lazy=1";
-    model.title=@"在生活中找寻游赏，也找寻写作 | 舒国治五城巡讲";
-    model.publisher=@"理想国、听道";
-    model.place=@"厦门·纸的时代书店";
-    model.date=@"2017-02-29";
-    
-    Activity *modelPhysicalActicity=[[Activity alloc]init];
-    modelPhysicalActicity.activityId=@"1";
-    modelPhysicalActicity.picUrl=@"https://imgsa.baidu.com/baike/c0%3Dbaike80%2C5%2C5%2C80%2C26/sign=cb319e894290f60310bd9415587bd87e/ac345982b2b7d0a24d471625c3ef76094a369aff.jpg";
-    modelPhysicalActicity.title=@"长沙地面 | 儿童精神分析系统培训工作坊（第一阶)";
-    modelPhysicalActicity.publisher=@" 大成心理工作室";
-    modelPhysicalActicity.place=@"长沙市星沙向阳路金科时代 3栋613  ";
-    modelPhysicalActicity.date=@"2017-02-29";
-    allDataFromServer=[[NSMutableArray alloc]init];
-    if([@"ylsl" isEqualToString:type])
-    for(int i=0;i<5;i++)
-        [allDataFromServer addObject:model];
-    else
-        for(int i=0;i<5;i++)
-            [allDataFromServer addObject:modelPhysicalActicity];
-    
-    
+
+//    Activity *model=[[Activity alloc]init];
+//    model.activityId=@"1";
+//    model.picUrl=@"http://mmbiz.qpic.cn/mmbiz_jpg/6qu8KwIJTLcqTxT8jqPOKCEAvGuhbFpzscsPC8FeDEYkQxsQ1AFSeJjYWgMeicQU2gJrgb3bhu7quicok24sLwIw/640?wx_fmt=jpeg&wxfrom=5&wx_lazy=1";
+//    model.title=@"在生活中找寻游赏，也找寻写作 | 舒国治五城巡讲";
+//    model.publisher=@"理想国、听道";
+//    model.place=@"厦门·纸的时代书店";
+//    model.date=@"2017-02-29";
+//    
+//    Activity *modelPhysicalActicity=[[Activity alloc]init];
+//    modelPhysicalActicity.activityId=@"1";
+//    modelPhysicalActicity.picUrl=@"https://imgsa.baidu.com/baike/c0%3Dbaike80%2C5%2C5%2C80%2C26/sign=cb319e894290f60310bd9415587bd87e/ac345982b2b7d0a24d471625c3ef76094a369aff.jpg";
+//    modelPhysicalActicity.title=@"长沙地面 | 儿童精神分析系统培训工作坊（第一阶)";
+//    modelPhysicalActicity.publisher=@" 大成心理工作室";
+//    modelPhysicalActicity.place=@"长沙市星沙向阳路金科时代 3栋613  ";
+//    modelPhysicalActicity.date=@"2017-02-29";
+//    allDataFromServer=[[NSMutableArray alloc]init];
+//    if([@"ylsl" isEqualToString:type])
+//    for(int i=0;i<5;i++)
+//        [allDataFromServer addObject:model];
+//    else
+//        for(int i=0;i<5;i++)
+//            [allDataFromServer addObject:modelPhysicalActicity];
+//    
+//
+
     // 2.集成刷新控件
     [self setupRefresh];
 }
@@ -187,6 +195,160 @@
     //跳转
     [self.navigationController pushViewController:nextPage animated:YES];
 }
+#pragma mark 加载活动列表
+-(void)loadData:(int)pageIndex orientation:(NSString *) orientation {
+    MBProgressHUD *hud;
+    hud = [MBProgressHUD showHUDAddedTo:self.view animated:YES];
+    //hud.color = [UIColor colorWithHexString:@"343637" alpha:0.5];
+    hud.labelText = @" 获取数据...";
+    [hud show:YES];
+    //获取全局ip地址
+    AppDelegate *myDelegate = [[UIApplication sharedApplication]delegate];
+    
+    NSString *urlString= [NSString stringWithFormat:@"%@/api/cms/activity/getList",myDelegate.ipString];
+    //创建数据请求的对象，不是单例
+    AFHTTPRequestOperationManager *manager=[AFHTTPRequestOperationManager manager];
+    //设置响应数据的类型,如果是json数据，会自动帮你解析
+    //注意setWithObjects后面的s不能少
+    manager.responseSerializer.acceptableContentTypes=[NSSet setWithObjects:@"application/json", nil];
+    NSString *token=myDelegate.token;
+    // 请求参数
+    NSDictionary *parameters = @{ @"appId":@"03a8f0ea6a",
+                                  @"appSecret":@"b4a01f5a7dd4416c",
+                                  @"channelType":type,
+                                  @"pageNumber":[NSString stringWithFormat:@"%d",pageIndex],
+                                  @"token":token
+                                  };
+    
+    [manager POST:urlString parameters:parameters success:^(AFHTTPRequestOperation *operation, id responseObject) {
+        // (最好在刷新表格后调用)调用endRefreshing可以结束刷新状态
+        [self.tableView footerEndRefreshing];
+        [self.tableView headerEndRefreshing];
+        //隐藏圆形进度条
+        [hud hide:YES];
+        NSString *result=[JsonUtil DataTOjsonString:responseObject];
+        NSLog(@"***************返回结果***********************");
+        NSLog(result);
+        NSData *data=[result dataUsingEncoding:NSUTF8StringEncoding];
+        NSError *error=[[NSError alloc]init];
+        NSDictionary *doc= [NSJSONSerialization JSONObjectWithData:data options:0 error:&error];
+        if(doc!=nil){
+            NSLog(@"*****doc不为空***********");
+            if([[doc objectForKey:@"code"] isKindOfClass:[NSNumber class]])
+                NSLog(@"code 是 NSNumber");
+            //判断code 是不是0
+            NSNumber *zero=[NSNumber numberWithInt:(0)];
+            NSNumber *code=[doc objectForKey:@"code"];
+            if([zero isEqualToNumber:code])
+            {
+                if(nil!=[doc allKeys]){
+                    
+                    NSArray *articleArray=[doc objectForKey:@"data"];
+                    if(0==[articleArray count]){
+                        
+                        if([orientation isEqualToString:@"down"])
+                            self.tableView.headerRefreshingText = @"亲，没有更多数据了";
+                        else
+                            self.tableView.footerRefreshingText = @"亲，没有更多数据了";
+                        UIAlertController *alert=[UIAlertController alertControllerWithTitle:nil message:@"抱歉,没有更多数据了" preferredStyle:UIAlertControllerStyleAlert];
+                        UIAlertAction *ok=[UIAlertAction actionWithTitle:@"确认"
+                                                                   style:UIAlertActionStyleDefault handler:nil];
+                        
+                        //        信息框添加按键
+                        [alert addAction:ok];
+                        [self presentViewController:alert animated:YES completion:nil];
+                        
+                    }
+                    else{
+                        
+                        for(NSDictionary *item in  articleArray ){
+                            Article *model=[[Article alloc]init];
+                            model.articleId=item [@"id"];
+                            model.picUrl=item [@"picurl"];
+                            model.title=item [@"title"];
+                            model.author=item [@"author"];
+                            
+                            //                    注意，publishat是NSNumber 类型的，所以不要用字符串去接收，否则报错
+                            //                    -[__NSCFNumber rangeOfCharacterFromSet:]: unrecognized selector sent to instance 0x7fa5216589d0"，对于IOS开发感兴趣的同学可以参考一下： 这个算是类型的不匹配，就是把NSNumber类型的赋给字符串了自己还不知情，
+                            
+                            model.date=item [@"publishat"];
+                            
+                            
+                            NSLog(@"******打印文章列表**********");
+                            NSLog(@"文章articleId是%@",model.articleId);
+                            NSLog(@"文章picUrl是%@",model.picUrl);
+                            NSLog(@"文章title是%@",model.title);
+                            NSLog(@"文章author是%@",model.author);
+                            NSLog(@"文章publishat是%i",model.date);
+                            //添加到数组以便显示到tableview
+                            NSLog(@"addObject之前");
+                            if([orientation isEqualToString:@"up"])
+                                [allDataFromServer addObject:model];
+                            else
+                                [allDataFromServer insertObject:model atIndex:0];
+                            NSLog(@"addObject之后");
+                        }
+                        NSLog(@"mDataArticle项数为%i",[allDataFromServer count]);
+                        NSLog(@"//更新界面");
+                        //更新界面
+                        [mTableView reloadData];
+                    }
+                    //            }
+                }
+                else
+                {
+                    UIAlertController *alert=[UIAlertController alertControllerWithTitle:nil message:@"抱歉，尚无文章可以阅读" preferredStyle:UIAlertControllerStyleAlert];
+                    UIAlertAction *ok=[UIAlertAction actionWithTitle:@"确认"
+                                                               style:UIAlertActionStyleDefault handler:nil];
+                    
+                    //        信息框添加按键
+                    [alert addAction:ok];
+                    [self presentViewController:alert animated:YES completion:nil];
+                    
+                }
+            }
+            
+            else{
+                
+                UIAlertController *alert=[UIAlertController alertControllerWithTitle:nil message:[doc objectForKey:@"msg"] preferredStyle:UIAlertControllerStyleAlert];
+                UIAlertAction *ok=[UIAlertAction actionWithTitle:@"确认"
+                                                           style:UIAlertActionStyleDefault handler:nil];
+                //        信息框添加按键
+                [alert addAction:ok];
+                [self presentViewController:alert animated:YES completion:nil];
+                
+            }
+            
+        }
+        else
+            NSLog(@"*****doc空***********");
+        
+        
+        
+        
+        
+        
+        
+        //        NSLog([self DataTOjsonString:responseObject]);
+        //          NSLog([self convertToJsonData:dic]);
+        
+    } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
+        [self.tableView footerEndRefreshing];
+        [self.tableView headerEndRefreshing];
+        //隐藏圆形进度条
+        [hud hide:YES];
+        NSString *errorUser=[error.userInfo objectForKey:NSLocalizedDescriptionKey];
+        if(error.code==-1009)
+            errorUser=@"主人，似乎没有网络喔！";
+        UIAlertController *alert=[UIAlertController alertControllerWithTitle:nil message:errorUser preferredStyle:UIAlertControllerStyleAlert];
+        UIAlertAction *ok=[UIAlertAction actionWithTitle:@"确认"
+                                                   style:UIAlertActionStyleDefault handler:nil];
+        
+        //        信息框添加按键
+        [alert addAction:ok];
+        [self presentViewController:alert animated:YES completion:nil];
+    }];}
+
 
 
 @end
