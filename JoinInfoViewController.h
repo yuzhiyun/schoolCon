8f2942640d6078d10620d40a85f3eb083d356cdd
qifeiqin <qifeiqin@gmail.com>
2017-03-02 17:45:23 +0800

    修改部分图片

---
+++ JoinInfoViewController.h
@@ -0,0 +1,13 @@
+//
+//  JoinInfoViewController.h
+//  SchoolCon
+//
+//  Created by 秦启飞 on 2017/3/2.
+//  Copyright © 2017年 dlz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JoinInfoViewController : UIViewController
+
+@end
